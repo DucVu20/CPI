cae08cf5ca1e6f28220fedf837fd955ee508a7f5
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2021-09-06 06:18:44 +0700

    Reformats for better readability.

--- src/main/scala/cpi/OV7670.scala
+++ src/main/scala/cpi/OV7670.scala
@@ -12,69 +12,68 @@ import freechips.rocketchip.tilelink._
 import freechips.rocketchip.util.UIntIsOneOf
 //import chipyard.iobinders.{OverrideIOBinder}
 
-
 case class CPIParams(
-  address: BigInt =0x10003000,
-  useAXI4: Boolean = false,
-  img_width: Int=640,
-  img_height: Int=480,
-  byte_per_pixel: Int =2,
-  SCCB_FREQ_kHz: Float=100,
-  CLK_FREQ_MHz: Float= 50,
-  pixel_width: Int=16
+  address: BigInt      =0x10003000,
+  useAXI4: Boolean     = false,
+  img_width: Int       = 640,
+  img_height: Int      = 480,
+  byte_per_pixel: Int  = 2,
+  SCCB_FREQ_kHz: Float = 100,
+  CLK_FREQ_MHz: Float  =  50,
+  pixel_width: Int     = 16
 )
 
 object CPIMOMI{
-  val cam_status=0x00   //wire
-  val cam_capture=0x04
-  val cam_mode=0x08
-  val cam_config=0x0C
-  val set_image_resolution=0x10
-  val returned_image_resolution=0x14
-  //val read_frame=0x18
-  val pixel=0x18
+  val cam_status                = 0x00   //wire
+  val cam_capture               = 0x04
+  val cam_mode                  = 0x08
+  val cam_config                = 0x0C
+  val set_image_resolution      = 0x10
+  val returned_image_resolution = 0x14
+  //val read_frame              = 0x18
+  val pixel                     = 0x18
 }
 
 class CPIIO(val img_width: Int, val img_height: Int,
              val byte_per_pixel: Int) extends Bundle{
 
-  val clock=Input(Clock())
-  val reset=Input(Bool())
+  val clock = Input(Clock())
+  val reset = Input(Bool())
 
-  val p_clk=Input(Bool())
-  val href=Input(Bool())
-  val vsync=Input(Bool())
-  val pixel_in=Input(UInt(8.W))
-  val pixel_out=Output(UInt((8*byte_per_pixel).W))          // 16 for ov7670
-  val pixel_addr=Output(UInt(log2Ceil((img_width*img_height)).W))
-  val frame_depth_in=Input(UInt(19.W))          // software
-  val frame_width=Output(UInt(log2Ceil(640).W)) // value read by hardware
-  val frame_height=Output(UInt(log2Ceil(480).W))
+  val p_clk          = Input(Bool())
+  val href           = Input(Bool())
+  val vsync          = Input(Bool())
+  val pixel_in       = Input(UInt(8.W))
+  val pixel_out      = Output(UInt((8*byte_per_pixel).W))          // 16 for ov7670
+  val pixel_addr     = Output(UInt(log2Ceil((img_width*img_height)).W))
+  val frame_depth_in = Input(UInt(19.W))          // software
+  val frame_width    = Output(UInt(log2Ceil(640).W)) // value read by hardware
+  val frame_height   = Output(UInt(log2Ceil(480).W))
 
-  val capture=Input(Bool())
-  val buffer_full=Output(Bool())  // valid
-  val read_frame=Input(Bool())    // ready
-  val capturing=Output(Bool())
+  val capture     = Input(Bool())
+  val buffer_full = Output(Bool())  // valid
+  val read_frame  = Input(Bool())    // ready
+  val capturing   = Output(Bool())
 
-  val config=Input(Bool())
-  val sccb_ready=Output(Bool())
-  val SIOC=Output(Bool())
-  val SIOD=Output(UInt(1.W))
-  val config_data=Input(UInt(8.W))
-  val control_address=Input(UInt(8.W))
+  val config          = Input(Bool())
+  val sccb_ready      = Output(Bool())
+  val SIOC            = Output(Bool())
+  val SIOD            = Output(UInt(1.W))
+  val config_data     = Input(UInt(8.W))
+  val control_address = Input(UInt(8.W))
 }
 trait CPIPortIO extends Bundle{
-  val SIOC=Output(Bool())
-  val SIOD=Output(UInt(1.W))
-  val p_clk=Input(Bool())
-  val href=Input(Bool())
-  val vsync=Input(Bool())
-  val pixel_in=Input(UInt(8.W))
+  val SIOC     = Output(Bool())
+  val SIOD     = Output(UInt(1.W))
+  val p_clk    = Input(Bool())
+  val href     = Input(Bool())
+  val vsync    = Input(Bool())
+  val pixel_in = Input(UInt(8.W))
  // val camera_clk=Output(Clock())  // clk for the camera
   // can use clock wizard to generate clock for the camera
 }
 class CPIInterrupts extends Bundle{
-  val new_frame=Bool()
+  val new_frame = Bool()
 }
 
 //class CPI(busWidthBytes: Int, val c: CPIParams)(implicit p:Parameters)
@@ -100,41 +99,44 @@ trait HasCPIIO extends BaseModule{
   val img_width: Int
   val img_height : Int
   val byte_per_pixel: Int
-  val io=IO(new CPIIO(img_width,img_height,byte_per_pixel))
+  val io = IO(new CPIIO(img_width,img_height,byte_per_pixel))
 }
 
 //trait CPITopIO extends Bundle{
 //  val new_frame=Output(Bool())
 //}
 
-class CPI(val img_width: Int,val img_height: Int,
-          val byte_per_pixel: Int, val CLK_FREQ_MHz: Float,
-          val SCCB_FREQ_kHz: Float) extends Module with HasCPIIO {
-  val capture_module=Module(new capture_module(img_width,img_height,
-                                            byte_per_pixel))
-  val SCCB_interface=Module(new SCCB_interface(CLK_FREQ_MHz,SCCB_FREQ_kHz))
+class CPI(val img_width: Int,
+  val img_height: Int,
+  val byte_per_pixel: Int,
+  val CLK_FREQ_MHz: Float,
+  val SCCB_FREQ_kHz: Float) extends Module with HasCPIIO {
 
-  capture_module.io.p_clk:=io.p_clk
-  capture_module.io.href:=io.href
-  capture_module.io.vsync:=io.vsync
-  capture_module.io.pixel_in:=io.pixel_in
-  io.pixel_out:=capture_module.io.pixel_out
-  io.pixel_addr:=capture_module.io.pixel_addr
-  capture_module.io.frame_depth_in:=io.frame_depth_in
-  io.frame_height:=capture_module.io.frame_height
-  io.frame_width:=capture_module.io.frame_width
+  val capture_module = Module(new capture_module(img_width, img_height, byte_per_pixel))
+  val SCCB_interface = Module(new SCCB_interface(CLK_FREQ_MHz, SCCB_FREQ_kHz))
 
-  capture_module.io.capture:=io.capture
-  io.capturing:=capture_module.io.capturing
-  io.buffer_full:=capture_module.io.buffer_full
-  capture_module.io.read_frame:=io.read_frame  // to read a frame, read frame signal must be high
+  capture_module.io.p_clk          := io.p_clk
+  capture_module.io.href           := io.href
+  capture_module.io.vsync          := io.vsync
+  capture_module.io.pixel_in       := io.pixel_in
+  capture_module.io.frame_depth_in := io.frame_depth_in
+  capture_module.io.capture        := io.capture
+  capture_module.io.read_frame     := io.read_frame  // to read a frame, read frame signal must be high
 
-  io.SIOC:=SCCB_interface.io.SIOC
-  io.SIOD:=SCCB_interface.io.SIOC
-  SCCB_interface.io.config:=io.config
-  io.sccb_ready:=SCCB_interface.io.sccb_ready
-  SCCB_interface.io.config_data:=io.config_data
-  SCCB_interface.io.control_address:=io.control_address
+  io.pixel_out    := capture_module.io.pixel_out
+  io.pixel_addr   := capture_module.io.pixel_addr
+  io.frame_height := capture_module.io.frame_height
+  io.frame_width  := capture_module.io.frame_width
+  io.capturing    := capture_module.io.capturing
+  io.buffer_full  := capture_module.io.buffer_full
+
+  io.SIOC       := SCCB_interface.io.SIOC
+  io.SIOD       := SCCB_interface.io.SIOC
+  io.sccb_ready := SCCB_interface.io.sccb_ready
+
+  SCCB_interface.io.config          := io.config
+  SCCB_interface.io.config_data     := io.config_data
+  SCCB_interface.io.control_address := io.control_address
 }
 trait CPIModule extends HasRegMap{
   val io: CPIPortIO
@@ -143,23 +145,22 @@ trait CPIModule extends HasRegMap{
   val clock: Clock
   val reset: Reset
 
-  val pixel=Wire(new DecoupledIO(UInt((params.byte_per_pixel*2).W)))
-  val CPI=Module(new CPI(params.img_width, params.img_height,
+  val pixel = Wire(new DecoupledIO(UInt((params.byte_per_pixel*2).W)))
+  val CPI   = Module(new CPI(params.img_width, params.img_height,
     params.byte_per_pixel, params.CLK_FREQ_MHz,params.SCCB_FREQ_kHz))
 
-  val status=Wire(UInt(3.W))
-  val capture_frame=WireInit(false.B)
-  val config=WireInit(false.B)
-  val camera_mode=WireInit(0.U(16.W))
-  val pixel_addr=Wire(UInt(16.W))
-  val returned_resolution=Wire(UInt(19.W))
-  val image_resolution=WireInit(0.U(19.W))
+  val status              = Wire(UInt(3.W))
+  val capture_frame       = WireInit(false.B)
+  val config              = WireInit(false.B)
+  val camera_mode         = WireInit(0.U(16.W))
+  val pixel_addr          = Wire(UInt(16.W))
+  val returned_resolution = Wire(UInt(19.W))
+  val image_resolution    = WireInit(0.U(19.W))
 
   CPI.io.p_clk    := io.p_clk
   CPI.io.href     := io.href
   CPI.io.vsync    := io.vsync
   CPI.io.pixel_in := io.pixel_in
-
 
   io.SIOC := CPI.io.SIOC
   io.SIOD := CPI.io.SIOC
@@ -174,13 +175,14 @@ trait CPIModule extends HasRegMap{
   CPI.io.frame_depth_in  := image_resolution
 
 
-  pixel.bits:=CPI.io.pixel_out
-  pixel.valid:=CPI.io.buffer_full
-  CPI.io.read_frame:=pixel.ready
-  pixel_addr:=CPI.io.pixel_addr
+  pixel.bits  := CPI.io.pixel_out
+  pixel.valid := CPI.io.buffer_full
+  pixel_addr  := CPI.io.pixel_addr
 
-  status:=Cat(CPI.io.sccb_ready, CPI.io.buffer_full, CPI.io.capturing)
-  returned_resolution:=Cat(CPI.io.frame_width,CPI.io.frame_height)
+  status := Cat(CPI.io.sccb_ready, CPI.io.buffer_full, CPI.io.capturing)
+
+  CPI.io.read_frame   := pixel.ready
+  returned_resolution := Cat(CPI.io.frame_width,CPI.io.frame_height)
 
   regmap(
     CPIMOMI.cam_status -> Seq(
@@ -249,19 +251,20 @@ trait CanHavePeripheryCPIModuleImp extends LazyModuleImp {
   val port = outer.cpi match {
     case  Some(cpi) => {
       val cpi_port=IO(new Bundle{
-        val SIOC=Output(Bool())
-        val SIOD=Output(Bool())
-        val vsync=Input(Bool())
-        val href=Input(Bool())
-        val pclk=Input(Bool())
-        val pixel_in=Input(UInt(8.W))
+        val SIOC     = Output(Bool())
+        val SIOD     = Output(Bool())
+        val vsync    = Input(Bool())
+        val href     = Input(Bool())
+        val pclk     = Input(Bool())
+        val pixel_in = Input(UInt(8.W))
       })
-      cpi_port.SIOC:=cpi.module.io.SIOC
-      cpi_port.SIOD:=cpi.module.io.SIOD
-      cpi.module.io.vsync:=cpi_port.vsync
-      cpi.module.io.href:=cpi_port.href
-      cpi.module.io.p_clk:=cpi_port.pclk
-      cpi.module.io.pixel_in:=cpi_port.pixel_in
+
+      cpi_port.SIOC          := cpi.module.io.SIOC
+      cpi_port.SIOD          := cpi.module.io.SIOD
+      cpi.module.io.vsync    := cpi_port.vsync
+      cpi.module.io.href     := cpi_port.href
+      cpi.module.io.p_clk    := cpi_port.pclk
+      cpi.module.io.pixel_in := cpi_port.pixel_in
 
       Some(cpi_port)
     }
--- src/main/scala/cpi/capture_module.scala
+++ src/main/scala/cpi/capture_module.scala
@@ -32,149 +32,149 @@ class capture_module(img_width:Int, img_height: Int,
                                      byte_per_pixel: Int) extends Module{
   // img_width=col, img_height=row
 
-  val width=img_width
-  val height=img_height
-  val frame_resolution=width*height   // the maximum depth of the buffer
-  val pixel_bit=8*byte_per_pixel
+  val width            = img_width
+  val height           = img_height
+  val frame_resolution = width * height   // the maximum depth of the buffer
+  val pixel_bit        = 8 * byte_per_pixel
 
-  val io=IO(new Bundle{
-    val p_clk=Input(Bool())
-    val href=Input(Bool())
-    val vsync=Input(Bool())
-    val pixel_in=Input(UInt(8.W))
-    val pixel_out=Output(UInt(pixel_bit.W))          // 16 for ov7670
-    val pixel_addr=Output(UInt(log2Ceil(frame_resolution).W))
-    val frame_depth_in=Input(UInt(log2Ceil(frame_resolution).W))    // the image depth or resolution (wxh) of a mode
+  val io = IO(new Bundle{
+    val p_clk          = Input(Bool())
+    val href           = Input(Bool())
+    val vsync          = Input(Bool())
+    val pixel_in       = Input(UInt(8.W))
+    val pixel_out      = Output(UInt(pixel_bit.W))          // 16 for ov7670
+    val pixel_addr     = Output(UInt(log2Ceil(frame_resolution).W))
+    val frame_depth_in = Input(UInt(log2Ceil(frame_resolution).W))    // the image depth or resolution (wxh) of a mode
     // This value needs to be known and set by software
-    val frame_width=Output(UInt(log2Ceil(640).W))
-    val frame_height=Output(UInt(log2Ceil(480).W))
-    val capture=Input(Bool())
-    val read_frame=Input(Bool())    // ready
-    val buffer_full=Output(Bool())  // valid
-    val capturing=Output(Bool())
+    val frame_width    = Output(UInt(log2Ceil(640).W))
+    val frame_height   = Output(UInt(log2Ceil(480).W))
+    val capture        = Input(Bool())
+    val read_frame     = Input(Bool())    // ready
+    val buffer_full    = Output(Bool())  // valid
+    val capturing      = Output(Bool())
   })
-  val idle::capture_frame::Nil=Enum(2)
+  val idle :: capture_frame :: Nil = Enum(2)
 
-  val FMS=RegInit(idle)
-  val depth_reg=RegInit(0.U(log2Ceil(frame_resolution).W))
-  val write_Ptr= RegInit(0.U(log2Ceil(frame_resolution).W))
-  val read_Ptr= RegInit(0.U(log2Ceil(frame_resolution).W))
+  val FMS       = RegInit(idle)
+  val depth_reg = RegInit(0.U(log2Ceil(frame_resolution).W))
+  val write_Ptr =  RegInit(0.U(log2Ceil(frame_resolution).W))
+  val read_Ptr  =  RegInit(0.U(log2Ceil(frame_resolution).W))
 
-  val full=RegInit(false.B)
-  val pixel_addr=RegNext(read_Ptr)
-  val pclk_risingEdge = (io.p_clk)& (!RegNext(io.p_clk))   // detect edge only when p_clock rises from low to high and href is true
-  val vsync_fallingEdge=(!io.vsync)&(RegNext(io.vsync))       // when vsync goes low, a new frame start, writePtr should be reset to 0
+  val full              = RegInit(false.B)
+  val pixel_addr        = RegNext(read_Ptr)
+  val pclk_risingEdge   =  (io.p_clk)& (!RegNext(io.p_clk))   // detect edge only when p_clock rises from low to high and href is true
+  val vsync_fallingEdge = (!io.vsync)&(RegNext(io.vsync))       // when vsync goes low, a new frame start, writePtr should be reset to 0
 
-  val first_byte=RegInit(0.U(8.W))
-  val second_byte=RegInit(0.U(8.W))
-  val pixel_val = Cat(second_byte,first_byte)     // RGB, MSB: R, LSB: B
+  val first_byte  = RegInit(0.U(8.W))
+  val second_byte = RegInit(0.U(8.W))
+  val pixel_val   =  Cat(second_byte,first_byte)     // RGB, MSB: R, LSB: B
 
-  val pixel_counter=RegInit(0.U(byte_per_pixel.W))
+  val pixel_counter = RegInit(0.U(byte_per_pixel.W))
 
-  val wrEna_wire=WireInit(false.B)
-  val wrEna=RegNext(wrEna_wire)
+  val wrEna_wire = WireInit(false.B)
+  val wrEna      = RegNext(wrEna_wire)
 
-  val capture_reg=RegInit(false.B)
-  capture_reg:=Mux(io.capture,io.capture,capture_reg)
+  val capture_reg = RegInit(false.B)
+  capture_reg := Mux(io.capture,io.capture,capture_reg)
 
-  val buffer=Module(new single_port_ram(frame_resolution,UInt((byte_per_pixel*8).W)))
+  val buffer = Module(new single_port_ram(frame_resolution,UInt((byte_per_pixel*8).W)))
 
-  val addr=WireInit(0.U(log2Ceil(frame_resolution).W))
+  val addr = WireInit(0.U(log2Ceil(frame_resolution).W))
 
-  val read_frame=io.read_frame&&full  // only read buffer when it's full and there's a signal
-  val capturing=WireInit(false.B)
+  val read_frame = io.read_frame&&full  // only read buffer when it's full and there's a signal
+  val capturing  = WireInit(false.B)
 
   //============================IO=========================================//
-  io.capturing:=capturing
-  buffer.io.wrEna:=wrEna
-  buffer.io.rdEna:=read_frame
-  buffer.io.addr:=addr
-  buffer.io.data_in:=pixel_val
-  depth_reg:=io.frame_depth_in
+  io.capturing      := capturing
+  buffer.io.wrEna   := wrEna
+  buffer.io.rdEna   := read_frame
+  buffer.io.addr    := addr
+  buffer.io.data_in := pixel_val
+  depth_reg         := io.frame_depth_in
 
   //==============SPECIFY RESOLUTION BASED ON HREF, VSYCN ==================//
-  val row_cnt=RegInit(0.U(log2Ceil(480).W))
-  val col_cnt=RegInit(0.U(log2Ceil(640).W))
+  val row_cnt = RegInit(0.U(log2Ceil(480).W))
+  val col_cnt = RegInit(0.U(log2Ceil(640).W))
 
   when((io.href)& (!RegNext(io.href))){
-    row_cnt:=row_cnt+1.U
-    col_cnt:=0.U
+    row_cnt := row_cnt+1.U
+    col_cnt := 0.U
   }
   when(vsync_fallingEdge){
-    row_cnt:=0.U
-    col_cnt:=0.U
+    row_cnt := 0.U
+    col_cnt := 0.U
   }
-  io.frame_width:=col_cnt
-  io.frame_height:=row_cnt
-  io.pixel_addr:=RegNext(read_Ptr)
-  io.pixel_out:=buffer.io.data_out
-  io.buffer_full:=full
+  io.frame_width  := col_cnt
+  io.frame_height := row_cnt
+  io.pixel_addr   := RegNext(read_Ptr)
+  io.pixel_out    := buffer.io.data_out
+  io.buffer_full  := full
 
   //==============READ ADDRESS GENERATOR==================//
   when(read_frame){
-    addr:=read_Ptr
-    read_Ptr:=read_Ptr+1.U
+    addr     := read_Ptr
+    read_Ptr := read_Ptr+1.U
     when(read_Ptr===(depth_reg-1.U)){
-      read_Ptr:=0.U
-      full:=false.B
+      read_Ptr := 0.U
+      full     := false.B
     }
   }otherwise{
-    addr:=RegNext(write_Ptr)
+    addr := RegNext(write_Ptr)
   }
 
   //====================FMS for capturing images============================//
   switch(FMS){
     is(idle){
       when(io.vsync){
-        FMS:=idle
+        FMS := idle
       }.otherwise{
         when(capture_reg){
           when(vsync_fallingEdge){
-            FMS:=capture_frame
-            capture_reg:=false.B
-            full:=false.B
+            FMS         := capture_frame
+            capture_reg := false.B
+            full        := false.B
           }
         }
       }
-      write_Ptr:=0.U
-      write_Ptr:=0.U
-      capturing:=false.B
+      write_Ptr := 0.U
+      write_Ptr := 0.U
+      capturing := false.B
     }
     is(capture_frame){
-      capturing:=true.B
-      FMS:=Mux(io.vsync,idle,capture_frame)
+      capturing := true.B
+      FMS := Mux(io.vsync,idle,capture_frame)
       when(pclk_risingEdge&&(io.href)){
-        pixel_counter:=pixel_counter+1.U
+        pixel_counter := pixel_counter+1.U
         switch(pixel_counter){
-          is(0.U) {first_byte:=io.pixel_in}
-          is(1.U) {second_byte:=io.pixel_in}
+          is(0.U) {first_byte := io.pixel_in}
+          is(1.U) {second_byte := io.pixel_in}
         }
         when(pixel_counter===(byte_per_pixel-1).U){
-          pixel_counter:=0.U
-          write_Ptr:=write_Ptr+1.U              // update pixel address
-          wrEna_wire:=pclk_risingEdge
-          col_cnt:=col_cnt+1.U
+          pixel_counter := 0.U
+          write_Ptr := write_Ptr+1.U              // update pixel address
+          wrEna_wire := pclk_risingEdge
+          col_cnt := col_cnt+1.U
         }
       }
       when(write_Ptr===(depth_reg-1.U)){
-        full:=true.B
+        full := true.B
       }
     }
   }
 }
 class single_port_ram[T<:Data](mem_depth: Int,
                                gen: T) extends Module{
-  val addr_width=log2Ceil(mem_depth)
-  val io=IO(new Bundle{
-    val addr=Input(UInt(addr_width.W))
-    val data_in=Input(gen)
-    val data_out=Output(gen)
-    val wrEna=Input(Bool())
-    val rdEna=Input(Bool())
+  val addr_width = log2Ceil(mem_depth)
+  val io = IO(new Bundle{
+    val addr     = Input(UInt(addr_width.W))
+    val data_in  = Input(gen)
+    val data_out = Output(gen)
+    val wrEna    = Input(Bool())
+    val rdEna    = Input(Bool())
   })
-  val mem=SyncReadMem(mem_depth,gen)
+  val mem = SyncReadMem(mem_depth,gen)
   when(io.wrEna){
     mem.write(io.addr,io.data_in)
   }
-  io.data_out:=mem.read(io.addr,io.rdEna)
+  io.data_out := mem.read(io.addr,io.rdEna)
 }
