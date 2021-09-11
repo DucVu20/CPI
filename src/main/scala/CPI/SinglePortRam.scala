94363d97c5f5a9e6878a2ddb1a3a785b851133f8
DucVu20 <leducvuvietnam@gmail.com>
2021-09-11 21:00:57 +0700

    rm unnecessary files

---
+++ src/main/scala/CPI/SinglePortRam.scala
@@ -0,0 +1,21 @@
+package CPI
+
+import chisel3._
+import chisel3.util._
+
+class single_port_ram[T <: Data](mem_depth: Int,
+                                 gen: T) extends Module {
+  val addr_width = log2Ceil(mem_depth)
+  val io = IO(new Bundle {
+    val addr     = Input(UInt(addr_width.W))
+    val data_in  = Input(gen)
+    val data_out = Output(gen)
+    val wrEna    = Input(Bool())
+    val rdEna    = Input(Bool())
+  })
+  val mem = SyncReadMem(mem_depth, gen)
+  when(io.wrEna) {
+    mem.write(io.addr, io.data_in)
+  }
+  io.data_out := mem.read(io.addr, io.rdEna)
+}
