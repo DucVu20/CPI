5d96f66b046a634b1e2302401e97bd1410fb407b
DucVu20 <leducvuvietnam@gmail.com>
2021-09-29 10:46:23 +0700

    test file for the CPI

--- src/test/scala/cpi/OV7670.c
+++ src/test/scala/cpi/OV7670.c
@@ -15,18 +15,21 @@
 #define PRESCALER                       (CPI_BASE_ADDR + 0x20)
 
 // com7 reg: address 0x12
-#define SCCB_REG_RESET_ALL (1 << 7)
-#define OUTPUT_QCIF (1 << 3)
-#define OUTPUT_QVGA (1 << 4)
-#define OUTPUT_CIF (1 << 5)
-#define OUTPUT_RGB (1 << 2)
-#define OUTPUT_RAW (1)
+#define REG_COM7    	0x12	/* Control 7 */
+#define COM7_RESET  	0x80	/* Register reset */
+#define COM7_FMT_MASK	0x38
+#define COM7_FMT_VGA	0x00
+#define	COM7_FMT_CIF	0x20	/* CIF format */
+#define COM7_FMT_QVGA	0x10	/* QVGA format */
+#define COM7_FMT_QCIF	0x08	/* QCIF format */
+#define	COM7_RGB	    0x04	/* bits 0 and 2 - RGB format */
+#define	COM7_YUV	    0x00	/* YUV */
+
 
 void configure_camera(unsigned int addr, unsigned int config_data){
     unsigned int mode = (addr << 8) | (config_data & 0xFF);
-    printf("Camera mode: %08X ADDR: %08X DATA: %08X\n", mode, addr, config_data);
+    printf("Camera mode: %08X \n", mode);
     reg_write16(CAM_MODE, mode)   ;   // write configuration to the camera
-
 }
 
 void capture_image() {
@@ -36,47 +39,41 @@ void capture_image() {
 
 void check_status(){
   printf("\n");
-  if((reg_read8(CAM_STATUS) & (0x2)) == 2){
+    if( (reg_read8(CAM_STATUS) & (0x01)) == 1){
+      printf("the camera is working\n");
+    }else{
+       printf("the camera is idle\n");
+    }
+
+    if((reg_read8(CAM_STATUS) & (0x2)) == 2){
         printf("buffer is full\n");
     }
     else{
         printf("buffer is empty\n");
     }
+
     if( (reg_read8(CAM_STATUS) & (0x04)) == 4){
-        printf("sccb interface is ready to configure a new mode\n");
+        printf("sccb interface is ready\n");
     }else{
-         printf("sccb interface is configuring a mode for the camera\n");
+         printf("sccb interface is busy\n");
     }
-    
-    if( (reg_read8(CAM_STATUS) & (0x01)) == 1){
-        printf("the camera is taking an image\n");
-    }else{
-         printf("the camera is idle\n");
-    }
+
     printf("\n");
-    
 }
+
 int main(void){
 
     reg_write8(PRESCALER, 4);   // divide clk to create XCLK
     printf("====================================================================");
     check_status();
 
-    while((reg_read8(CAM_STATUS) & 0x04) == 1){
-      printf("configuring\n");
-    }    ; // wait for sccb to ready
-
-    configure_camera(0x12, SCCB_REG_RESET_ALL) ;   // reset the camera to default mode
     printf("Reset the camera ");
-    check_status();
-
-    while((reg_read8(CAM_STATUS) & 0x04) == 0);        // wait until sccb interface is ready
-    
-    configure_camera(0x40, 0xD0);   // RGB565
+    configure_camera(REG_COM7, COM7_RESET) ;   // reset the camera to default mode
     printf("Configured the RGB565 mode \n");
+    configure_camera(0x40, 0xD0);   // RGB565
+    check_status();
+    while((reg_read8(CAM_STATUS) & 0x04) == 0); // wait until the previous mode is configured
 
-    while((reg_read8(CAM_STATUS) & 0x04) == 0);        // wait until sccb interface is ready
-    
     printf("switch to RGB image acquisition mode\n");
     reg_write8(GRAY_IMAGE, 0);
 
@@ -84,8 +81,7 @@ int main(void){
     printf("\n");
     capture_image();    // capture image
 
-    //    while((reg_read8(CAM_STATUS) & 0x04) == 0);        // wait until sccb interface is ready
-    while((reg_read8(CAM_STATUS) & 0x01) == 0 ); // wait util the camra is capturing
+    while((reg_read8(CAM_STATUS) & 0x01) == 0 ); // wait util the camera is capturing
 
     check_status();
 
@@ -93,7 +89,7 @@ int main(void){
       //      printf(" Height: %08d\n ", reg_read32(RETURN_IMAGE_HEIGHT));
       //      printf(" Width: %08d\n  ", reg_read32(RETURN_IMAGE_WIDTH));
       //    }
-    while((reg_read8(CAM_STATUS) & 0x02) == 0);
+    while((reg_read8(CAM_STATUS) & 0x02) == 0); // wait till the buffer is full
     check_status();
 
     //    while((reg_read8(CAM_STATUS) & 0x02) == 2){
@@ -104,33 +100,46 @@ int main(void){
     printf("Returned image width: %08d\n\n", reg_read32(RETURN_IMAGE_WIDTH));
     check_status();
 
-    
-    printf("configure the RGB-CIF resolution 352x288\n\n");
-    while((reg_read8(CAM_STATUS) & 0x04) == 0); // wait for sccb to be ready
-    configure_camera(0x12, OUTPUT_CIF | OUTPUT_RGB);
+    printf("configure the RGB-CIF resolution 352x288\n");
+    configure_camera(REG_COM7, COM7_FMT_CIF);
+    while((reg_read8(CAM_STATUS) & 0x04) == 0); // wait until it finishes
     check_status();
-    while((reg_read8(CAM_STATUS) & 0x04) == 0); // wait for sccb to be ready
-    check_status();
-    capture_image();
-    
+
     printf("take a second shot\n");
+    capture_image();
     while((reg_read8(CAM_STATUS) & 0x02) == 0 ); // wait until the buffer is full
     printf("the resolution of the second shot is\n");
     printf("Returned image height: %08d\n", reg_read32(RETURN_IMAGE_HEIGHT));
     printf("Returned image width: %08d\n", reg_read32(RETURN_IMAGE_WIDTH));
     printf("\n");
-    
+
+    printf("take a third RGB-CIF shot\n");
     capture_image();
     while((reg_read8(CAM_STATUS) & 0x02) == 0 ); // wait until the buffer is full
     printf("the resolution of the third shot is\n");
-    printf("\n");
     printf("Returned image height: %08d\n", reg_read32(RETURN_IMAGE_HEIGHT));
     printf("Returned image width: %08d\n", reg_read32(RETURN_IMAGE_WIDTH));
+    printf("\n");
+
+    printf("configure QCIF: 176x144\n");
+    configure_camera(REG_COM7, COM7_FMT_QCIF);
+    while((reg_read8(CAM_STATUS) & 0x04) == 0); // wait until it finishes
 
     capture_image();
+    printf("take a fourth shot QCIF \n");
     while((reg_read8(CAM_STATUS) & 0x02) == 0 ); // wait until the buffer is full
     printf("the resolution of the fourth shot is\n");
     printf("Returned image height: %08d\n", reg_read32(RETURN_IMAGE_HEIGHT));
     printf("Returned image width: %08d\n", reg_read32(RETURN_IMAGE_WIDTH));
-    
+    printf("\n");
+
+    printf("reset to default mode, expected resolution: 640x480\n");
+    configure_camera(REG_COM7, COM7_RESET) ;   // reset the camera to default mode
+    while((reg_read8(CAM_STATUS) & 0x04) == 0); // wait until the previous mode is configured
+    capture_image();
+    while((reg_read8(CAM_STATUS) & 0x02) == 0 ); // wait until the buffer is full
+    check_status();
+    printf("Returned image height: %08d\n", reg_read32(RETURN_IMAGE_HEIGHT));
+    printf("Returned image width: %08d\n", reg_read32(RETURN_IMAGE_WIDTH));
+
 }
