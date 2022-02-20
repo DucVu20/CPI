76972a1fbf88ee886bced176fa9efbfcfed16522
DucVu20 <leducvuvietnam@gmail.com>
2022-02-20 11:10:55 +0700

    move python script from test/resources to python folder

---
+++ python/recover_image.py
@@ -0,0 +1,29 @@
+# This python script is used for displaying gray images from a .txt pixel stream
+# file acquired by the SOC
+
+from PIL import Image, ImageOps
+import numpy as np
+
+def RecoverGrayImage(sourcePath, height, width):
+    grayImage = []
+    with open(sourcePath, 'r') as source:
+        for line in source:
+            for word in line.split():
+                if (len(word) == 8):
+                    grayImage.append(int(word[0:4], 16))
+                    grayImage.append(int(word[4:8], 16))
+                else:
+                    grayImage.append(int(word, 16))
+                    
+    grayImage = np.array(grayImage)
+    grayImage.astype(np.uint8)
+    grayImage = np.reshape(grayImage, (height, width))
+    print("resized image size: ", grayImage.shape)
+    print("the first 5 cols are: ", grayImage[:,0:5])
+    print("the length of the image is ", grayImage.size)
+    grayImage = Image.fromarray(grayImage)
+    grayImage.save(sourcePath+'OV7670Gray.png')
+    grayImage.show()    
+    
+sourcePath = "C:/Users/leduc/Downloads/290_352/"
+RecoverGrayImage(sourcePath, 290, 352)
